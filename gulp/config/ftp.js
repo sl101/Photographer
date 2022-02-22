2f2ba69a5b99c1d9723ef713041577b5003f0d5f
V Zhevaha <v.zhevaga@gmail.com>
2022-02-22 06:31:52 -0500

    tab в header

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-	host: "", // Адрес FTP сервера
-	user: "", // Имя пользователя
-	password: "", // Пароль
+	host: "zhevaga.beget.tech", // Адрес FTP сервера
+	user: "zhevaga_photographer", // Имя пользователя
+	password: "SL101v4slav!", // Пароль
 	parallel: 5 // Кол-во одновременных потоков
 }
