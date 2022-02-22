a9e730c5bac6b2525eec71ab8cc917d9366ac62e
V Zhevaha <v.zhevaga@gmail.com>
2022-02-22 04:46:21 -0500

    hero section + adaptive

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,10 +1,8 @@
 console.log("Hello Gulp!");
 
 import * as flsFunctions from "./modules/functions.js";
-
 flsFunctions.isWebp();
 
 import * as swipers from "./modules/swipers.js";
-
 import * as scripts from "./files/scripts.js";
 import * as dynamicAdaptive from "./files/d_adaptiv.js";
