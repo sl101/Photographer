e1aa8b6a6408c14e43b372768462ee88def70bfd
V Zhevaha <v.zhevaga@gmail.com>
2022-02-23 04:46:46 -0500

    fixed burger.js

--- #src/js/app.js
+++ #src/js/app.js
@@ -3,6 +3,7 @@ console.log("JS start!");
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
+import * as burger from "./files/burger.js";
 import * as swipers from "./modules/swipers.js";
 import * as scripts from "./files/scripts.js";
 import * as dynamicAdaptive from "./files/d_adaptiv.js";
