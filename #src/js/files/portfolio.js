8b6fcbe1f6b1497e668536b0e3d9fc9e62efc995
V Zhevaha <v.zhevaga@gmail.com>
2022-02-27 06:22:11 -0500

    added portfolio

--- #src/js/files/portfolio.js
+++ #src/js/files/portfolio.js
@@ -1,21 +1,12 @@
-const buttonOpen = document.querySelector('.portfolio__button--open');
-const buttonClose = document.querySelector('.portfolio__button--close');
-const porfolioContent = document.querySelector('.portfolio__content');
+const buttonOpen = document.querySelector('.portfolio__button');
 
 buttonOpen.addEventListener('click', function (event) {
-// console.log("click");
-buttonOpen.style.opacity = "0";
-buttonOpen.style.visibility = "hidden";
-buttonClose.style.opacity = "1";
-buttonClose.style.visibility = "visible";
-porfolioContent.classList.add('active');
-});
-
-buttonClose.addEventListener('click', function (event) {
-// console.log("click");
-buttonOpen.style.opacity = "1";
-buttonOpen.style.visibility = "visible";
-buttonClose.style.opacity = "0";
-buttonClose.style.visibility = "hidden";
-porfolioContent.classList.remove('active');
+if(buttonOpen.classList.contains('active')){
+	buttonOpen.classList.remove('active');
+	buttonOpen.textContent = "Скрыть";
+}
+else{
+	buttonOpen.classList.add('active');
+	buttonOpen.textContent = "Смотреть больше";
+}
 });
